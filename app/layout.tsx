6eeeae6c18d3fb20abb3dfdc0858295b8845a046
rosch-arthur <rosch.arthur@gmail.com>
2024-11-05 18:27:46 -0300

    fxi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,6 @@
 import './globals.css'
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import { ToastContainer } from 'react-toastify';
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+       
       >
         <ToastContainer pauseOnFocusLoss={false} />
         {children}
